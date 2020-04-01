da60ca608e9201cba2eaa8a590d68fd5221f889f
K.Sielski <kris@sielski.com>
2020-04-01 18:24:15 -0400

    Initial Commit, Intro and Lesson 1

---
+++ Lessons/Lesson1.py
@@ -0,0 +1,46 @@
+
+# Comment
+
+print("Hello")  #This is a comment too
+
+
+#variables
+num_students = 10
+euler = 2.7182
+myname = "Kris"
+class_name = "Python Programming is cool!!!!"
+
+print( num_students, euler, "String", myname, class_name )
+
+##Inputs
+##
+###input << Keyword
+##value = input("What is your name: ")
+##
+##print(value)
+##
+##age = eval(input("What is your age?: "))
+##
+##print(age  + 10)
+
+
+
+#
+s1 = "String1"
+s2 = "string2"
+
+s3 = s1+s2
+
+#print(s3)
+
+print(len(s3))
+
+#slicing
+print(   s3[7:8] )
+
+
+## Assignment:  Write a Python program to ask your full name and age and print them out.
+## Then, slice out and print only your middle name.
+
+
+
