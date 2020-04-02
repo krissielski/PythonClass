3c63c23b901f45b0621deebea28db60382875401
K.Sielski <kris@sielski.com>
2020-04-02 17:24:33 -0400

    Lesson 2

---
+++ Lessons/Assignment1.py
@@ -0,0 +1,21 @@
+#Assignment 1:  Guessing game
+
+#randomly generate number between 0 and 100
+from random import random
+num_to_guess =  int(random()*100)
+
+
+correct_guess = False
+
+while( not correct_guess ):
+
+    #Ask for guess
+    #Check for valid input
+    #Check if too high/low
+    #Exit loop if correct
+    pass
+    
+
+
+
+
---
+++ Lessons/Lesson2.py
@@ -0,0 +1,48 @@
+#Lesson 2:  IF statements
+
+
+#booleans = True/False
+this_is_true = True
+this_is_false = False
+
+
+num = 12
+
+if( num == 10 ):
+    print("was true")
+
+elif( num==11 ):
+    print("was 11")
+
+else:
+    print("Not True")
+    
+#if
+#if-else
+#if-elif-else     
+
+
+# =   <- Assignment   ex:   num =  10   (num now equals 10)
+# ==  <- Evaluation   ex:   num == 10   (returns True/False)
+
+
+# Comparisons
+#   ==  is equal
+#   !=  not equal
+#   <   is less than
+#   <=  is lessthan or equal
+#   >   is greater than
+#   >=  is greater than or equal
+
+#Loops
+#While Loop
+
+count = 0
+while( count < 10 ) :
+
+    #Tabbed
+    print("count", count)
+    count = count + 1
+    
+
+print("Done")
