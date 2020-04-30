1302682087ec30116be4d2ea4f79aec4306c7889
K.Sielski <kris@sielski.com>
2020-04-30 19:22:56 -0400

    Lesson 10

---
+++ Lessons/Lesson10.py
@@ -0,0 +1,39 @@
+#Lesson10 - Dictionary
+
+#list = []
+#touple = ()
+#dictionary = {}
+
+
+days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
+
+#empty dict
+emptydict = {}
+
+#dictionary = { key : value }
+
+
+days_in_month = { 'jan':31, 'feb':28, 'mar':31, 'apr':30 , 'may':31 }
+
+
+days_in_month[ 'jun'] = 30
+
+
+#can't sort
+#  list -> order is important
+#  dict -> getting data without knowing location
+
+#
+
+data = {}
+data[ 'k1'] = 100
+
+data['k2'] = 200
+
+
+myList = [1,2,3,4,5]
+
+
+data['k3'] = myList
+
+
